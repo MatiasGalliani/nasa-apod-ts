2c4ce586026a75faa08f1c62116f6e68f999c94a
MatiasGalliani <matiasgalliani00@gmail.com>
2026-07-22 16:56:18 -0300

    added cors

--- backend/index.ts
+++ backend/index.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 
 const app = express();
 const PORT = 8000;
-const originAllowed = process.env.ORIGIN_ALLOWED;
+const originAllowed = process.env.ALLOWED_ORIGIN;
 
 app.use(
   cors({
