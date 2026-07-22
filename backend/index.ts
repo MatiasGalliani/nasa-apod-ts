0bef47d5d0157b7a23149bade5d5d91a7af2ed43
MatiasGalliani <matiasgalliani00@gmail.com>
2026-07-22 14:44:05 -0300

    first commit

---
+++ backend/index.ts
@@ -0,0 +1,48 @@
+import "dotenv/config";
+import express, { type Request, type Response } from "express";
+
+const app = express();
+
+const PORT = 8000;
+
+type Apod = {
+  title: string;
+  explanation: string;
+  url: string;
+  date: string;
+};
+
+async function obtainAPOD() {
+  if (!process.env.NASA_API_KEY) {
+    throw new Error("The API key is missing");
+  }
+
+  const apiKey: string = process.env.NASA_API_KEY;
+
+  const response = await fetch(
+    `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`,
+  );
+
+  if (!response.ok) {
+    throw new Error("There was an error with the response");
+  }
+
+  const apod: Apod = await response.json();
+
+  return apod;
+}
+
+app.get("/apod", async (req: Request, response: Response) => {
+  try {
+    const apod = await obtainAPOD();
+    response.json(apod);
+  } catch (error) {
+    if (error instanceof Error) {
+      response.status(500).json({ error: error.message });
+    }
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
